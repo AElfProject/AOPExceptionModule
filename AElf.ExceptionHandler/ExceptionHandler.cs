c861dacb19e042a8f6cb25fae4feca7f848864aa
zhifenglee-aelf <zhifeng.lee@aelf.io>
2024-10-28 15:25:12 +0800

    feat(throw): fixed type issue with throw

--- AElf.ExceptionHandler/ExceptionHandler.cs
+++ AElf.ExceptionHandler/ExceptionHandler.cs
@@ -275,7 +275,7 @@ namespace AElf.ExceptionHandler
 
             if (flowBehavior.ExceptionHandlingStrategy == ExceptionHandlingStrategy.Throw)
             {
-                if (args.MethodInfo.ReturnType != typeof(Exception))
+                if (!typeof(Exception).IsAssignableFrom(args.MethodInfo.ReturnType))
                 {
                     throw new ReturnTypeMismatchException(
                         "Return type mismatch when trying to throw a new exception. ReturnValue should be of type Exception.");
